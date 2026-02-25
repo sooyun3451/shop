c6bed75f29e9283727f9a1eae81a5a64966cff30
sooyun3451 <sooyun020820@gmail.com>
2026-02-25 17:04:23 +0900

    deploy추가

---
+++ scripts/start-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "--------------- 서버 배포 시작 -----------------"
+
+REGION=ap-northeast-2
+ACCOUNT=992382458231
+REPO=market-server
+IMAGE=$ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$REPO:latest
+
+# ECR 로그인
+aws ecr get-login-password --region $REGION \
+| docker login --username AWS --password-stdin $ACCOUNT.dkr.ecr.$REGION.amazonaws.com
+
+# 기존 컨테이너 제거
+docker stop $REPO || true
+docker rm $REPO || true
+
+# 이미지 pull
+docker pull $IMAGE
+
+# 컨테이너 실행
+docker run -d --name $REPO -p 8080:8080 $IMAGE
+
+echo "--------------- 서버 배포 끝 -----------------"
